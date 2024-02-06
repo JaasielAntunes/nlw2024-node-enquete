81eb466ad102e376228cc0420f6f715cbb738951
Jaasiel <69393570+JaasielAntunes@users.noreply.github.com>
2024-02-06 15:47:21 -0300

    Salvando voto do usuário no Redis database para ficar em cache

--- src/http/routes/vote-on-poll.ts
+++ src/http/routes/vote-on-poll.ts
@@ -1,6 +1,9 @@
 import { z } from "zod";
 import { FastifyInstance } from "fastify";
 import { randomUUID } from "node:crypto";
+import { prisma } from "../../lib/prisma";
+import { voting } from "../../utils/voting-pub-sub";
+import { redis } from "../../lib/redis";
 
 export async function voteOnPoll(app: FastifyInstance) {
   app.post("/polls/:pollId/votes", async (req, res) => {
@@ -17,6 +20,43 @@ export async function voteOnPoll(app: FastifyInstance) {
 
     let { sessionId } = req.cookies;
 
+    if (sessionId) {
+      const userPreviousVoteOnPoll = await prisma.vote.findUnique({
+        where: {
+          sessionId_pollId: {
+            sessionId,
+            pollId,
+          },
+        },
+      });
+
+      if (
+        userPreviousVoteOnPoll &&
+        userPreviousVoteOnPoll.pollOptionId !== pollOptionId
+      ) {
+        await prisma.vote.delete({
+          where: {
+            id: userPreviousVoteOnPoll.id,
+          },
+        });
+
+        const votes = await redis.zincrby(
+          pollId,
+          -1,
+          userPreviousVoteOnPoll.pollOptionId
+        );
+
+        voting.publish(pollId, {
+          pollOptionId: userPreviousVoteOnPoll.pollOptionId,
+          votes: Number(votes),
+        });
+      } else if (userPreviousVoteOnPoll) {
+        return res
+          .status(400)
+          .send({ message: "Você já votou nesta enquete!" });
+      }
+    }
+
     if (!sessionId) {
       sessionId = randomUUID();
 
@@ -28,6 +68,21 @@ export async function voteOnPoll(app: FastifyInstance) {
       });
     }
 
-    return res.status(201).send({ sessionId });
+    await prisma.vote.create({
+      data: {
+        sessionId,
+        pollId,
+        pollOptionId,
+      },
+    });
+
+    const votes = await redis.zincrby(pollId, 1, pollOptionId);
+
+    voting.publish(pollId, {
+      pollOptionId,
+      votes: Number(votes),
+    });
+
+    return res.status(201).send();
   });
 }
