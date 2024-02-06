aa3199e42ca4dd5e7d0c4af8132909a7839e3c5b
Jaasiel <69393570+JaasielAntunes@users.noreply.github.com>
2024-02-06 12:38:38 -0300

    Rota de cadastrar enquete

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,11 +1,26 @@
-import fastify from 'fastify';
+import fastify from "fastify";
+import { PrismaClient } from "@prisma/client";
+import { z } from "zod";
 
 const app = fastify();
+const prisma = new PrismaClient();
 
-app.get('/', () => {
-  return {hello: 'world'}
-})
+app.post("/polls", async (req, res) => {
+  const createPollBody = z.object({
+    title: z.string(),
+  });
 
-app.listen({port: 3333}).then(() => {
-  console.log('Server running on http://localhost:3333');
+  const { title } = createPollBody.parse(req.body);
+
+  const poll = await prisma.poll.create({
+    data: {
+      title,
+    },
+  });
+
+  return res.status(201).send({ pollId: poll.id });
+});
+
+app.listen({ port: 3333 }).then(() => {
+  console.log("Server running on http://localhost:3333");
 });
