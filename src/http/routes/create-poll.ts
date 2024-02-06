1000f8752e733bd2178159cdfe5a05b2c57ec266
Jaasiel <69393570+JaasielAntunes@users.noreply.github.com>
2024-02-06 14:22:13 -0300

    Cadastro de enquete com opções

--- src/http/routes/create-poll.ts
+++ src/http/routes/create-poll.ts
@@ -1,4 +1,4 @@
-import z from "zod";
+import { z } from "zod";
 import { prisma } from "../../lib/prisma";
 import { FastifyInstance } from "fastify";
 
@@ -6,16 +6,26 @@ export async function createPoll(app: FastifyInstance) {
   app.post("/polls", async (req, res) => {
     const createPollBody = z.object({
       title: z.string(),
+      options: z.array(z.string()),
     });
-  
-    const { title } = createPollBody.parse(req.body);
-  
+
+    const { title, options } = createPollBody.parse(req.body);
+
     const poll = await prisma.poll.create({
       data: {
         title,
+        options: {
+          createMany: {
+            data: options.map((option) => {
+              return {
+                title: option,
+              };
+            }),
+          },
+        },
       },
     });
-  
+
     return res.status(201).send({ pollId: poll.id });
   });
 }
