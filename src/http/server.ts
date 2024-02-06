c015279666c6f5f49a039a4d308963c57545f1b0
Jaasiel <69393570+JaasielAntunes@users.noreply.github.com>
2024-02-06 11:16:09 -0300

    Projeto criado e teste de rota

---
+++ src/http/server.ts
@@ -0,0 +1,11 @@
+import fastify from 'fastify';
+
+const app = fastify();
+
+app.get('/', () => {
+  return {hello: 'world'}
+})
+
+app.listen({port: 3333}).then(() => {
+  console.log('Server running on http://localhost:3333');
+});
